8f6920814b93c86f4a133c2d8d24c9957a2e750a
Bear-software <linner_952@mail.ru>
2024-12-09 11:07:20 +0100

    refactoring header & footer

--- src/lib/shared/ui/Layout.svelte
+++ src/lib/shared/ui/Layout.svelte
@@ -1,21 +1,12 @@
 <script lang="ts">
-	import { Header, Footer } from '$widgetsLanding';
-	import { navigation, categories, boardgamesList } from '$sharedData';
 	import '../../../app/app.css';
 
 	let { children } = $props();
 </script>
 
-<div class="page-wrapper">
-	<Header {navigation} {boardgamesList}/>
-
-	<main>
-		{@render children()}
-	</main>
-
-	<Footer {navigation} {categories}/>
-</div>
-<div class="bg-gradient"></div>
+<main class="overflow-hidden">
+	{@render children()}
+</main>
 
 <style lang="postcss">
 </style>
