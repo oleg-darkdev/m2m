e47f99dd59e6efe860d3638123f77fc95bc7950f
Bear-software <linner_952@mail.ru>
2024-12-09 11:08:47 +0100

    basic structure

---
+++ src/routes/[[lang=lang]]/app/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	// import {} from '$sharedData';
+	// import {} from '$sharedUi';
+	import { List } from '$widgetsLanding';
+	// import { SEO } from '$sharedUtils';
+	import { FooterNav } from '$entitiesApp';
+</script>
+
+<svelte:head>
+	<title>Минута в минуту | App</title>
+	<meta name="description" content="" />
+</svelte:head>
+
+<!-- <SEO title="Home" /> -->
+
+<List />
+
+<FooterNav />
+
+
+<style lang="postcss">
+	
+</style>
