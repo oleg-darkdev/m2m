5536c8d25ebe565fa38b2f7af00b8562da9e49cd
Bear-software <linner_952@mail.ru>
2025-01-02 20:16:21 +0100

    upld nav data

--- src/lib/shared/data/index.ts
+++ src/lib/shared/data/index.ts
@@ -1,5 +1,18 @@
+import navigationLanding from './navigationLanding';
+import navigationMenu from './navigationMenu';
+
 import socialLinks from './socialLinks';
 import donateLinks from './donateLinks';
 import cryptoLinks from './cryptoLinks';
+// import navigation from './navigation';
+// import navigation from './navigation';
+// import navigation from './navigation';
+// import navigation from './navigation';
 
-export { socialLinks, donateLinks, cryptoLinks };
+export {
+	navigationLanding,
+	navigationMenu,
+	socialLinks,
+	donateLinks,
+	cryptoLinks,
+};
